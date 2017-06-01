9d309287e157a5084ace4932e67034d41d9ff185
Andrew Hudson <andrew.hudson@informa.com>
2017-06-01 15:50:57 +0100

    make some changes

--- server.js
+++ server.js
@@ -15,41 +15,71 @@ var wp = new WPAPI({
 var io = require('socket.io')(port);
 var redis = require('socket.io-redis');
 io.adapter(redis({ host: 'localhost', port: 6379 }));
-
+const filePath = './images/sun.png';
 
 io.on('connection', function(socket) {
 	console.log('connected to the server');
 
-  	socket.on('published new post', function(id) {
-    	console.log('published new post', id);
-
-		// Create the media record & upload your image file
-		const filePath = './images/owl.jpg';
-		return wp.media().file( filePath ).create({
-			title: 'Amazing featured image',
-			post: id
-		})
-			.then(function( media ) {
-			// Set the new media record as the post's featured media
-				return wp.posts().id( id ).update({
-					featured_media: media.id,
-					status: 'publish',
-					content: 'some lovely content'
-				});
-			})
-			.then(() => {
-				socket.emit('post complete');
-			});
-
-
+  	socket.on('browser published new post', function(id, post, update) {
+  		console.log('update post');
+   //  	console.log('published new post', id, post, update);
+   		addFeaturedImageToPost(id, socket);
   	});
 
 });
 
-// io.on('published new post', function(msg) {
-//   console.log('published new post', msg);
-// });
 
-// io.on('img clicked', (msg) => {
-//   console.log('Image was clicked', msg);
-// })
+const addFeaturedImageToPost = function(id, socket) {
+	// // Create the media record & upload your image file
+	// return wp.media().file( filePath ).create({
+	// 	title: 'Amazing featured image',
+	// 	post: id
+	// })
+	// 	.then(function( media ) {
+	// 		console.log('have uploaded the image');
+	// 	// Set the new media record as the post's featured media
+	// 		return wp.posts().id( id ).update({
+	// 			featured_media: media.id,
+	// 			status: 'publish',
+	// 			content: 'some lovely content'
+	// 		});
+	// 	})
+	// 	.then(() => {
+	// 		console.log('post has been updated with featured image')
+	// 		// return socket.emit('post complete');
+	// 	});
+	// let done = false;
+	return wp.media()
+	    // Specify a path to the file you want to upload, or a Buffer
+	    .file( filePath )
+	    .create({
+	        title: 'My awesome image',
+	        alt_text: 'an image of something awesome',
+	        caption: 'This is the caption text',
+	        description: 'More explanatory information'
+	    })
+	    .then(function(response) {
+	        // Your media is now uploaded: let's associate it with a post
+	        // var newImageId = response.id;
+	        // return wp.media().id( newImageId ).update({
+	        //     post: id
+	        // });
+	        var newImageId = response.id;
+
+	        return wp.posts().id(id).update({
+    				featured_media: newImageId,
+    				status: 'publish',
+    				content: 'some lovely content'
+    			})
+	        	.then(function(response) {
+	        		console.log(response);
+	        		socket.emit('post complete');
+	        	});
+
+	    })
+	    .then(function( response ) {
+	            console.log( 'Media ID #' + response );
+	            // console.log( 'is now associated with Post ID #' + response.post );
+	        }
+	    );
+};

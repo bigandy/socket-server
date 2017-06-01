b6fc0865adbea7cb95e2cbea8054b326d8ed7208
Andrew Hudson <ahudson@gmail.com>
2017-06-01 17:36:43 +0100

    seems not to loop (for now)

--- server.js
+++ server.js
@@ -5,53 +5,32 @@ var WPAPI = require( 'wpapi' );
 const config = require('./config.json');
 
 var wp = new WPAPI({
-    endpoint: config.endpoint,
-    username: config.username,
-    password: config.password,
-    auth: true
-});
-
+    endpoint,
+    username,
+    password,
+    auth
+} = config);
 
 var io = require('socket.io')(port);
 var redis = require('socket.io-redis');
 io.adapter(redis({ host: 'localhost', port: 6379 }));
-const filePath = './images/sun.png';
+
+const filePath = './images/owl.jpg';
 
 io.on('connection', function(socket) {
 	console.log('connected to the server');
 
   	socket.on('browser published new post', function(id, post, update) {
   		console.log('update post');
-   //  	console.log('published new post', id, post, update);
    		addFeaturedImageToPost(id, socket);
   	});
-
 });
 
 
 const addFeaturedImageToPost = function(id, socket) {
-	// // Create the media record & upload your image file
-	// return wp.media().file( filePath ).create({
-	// 	title: 'Amazing featured image',
-	// 	post: id
-	// })
-	// 	.then(function( media ) {
-	// 		console.log('have uploaded the image');
-	// 	// Set the new media record as the post's featured media
-	// 		return wp.posts().id( id ).update({
-	// 			featured_media: media.id,
-	// 			status: 'publish',
-	// 			content: 'some lovely content'
-	// 		});
-	// 	})
-	// 	.then(() => {
-	// 		console.log('post has been updated with featured image')
-	// 		// return socket.emit('post complete');
-	// 	});
-	// let done = false;
 	return wp.media()
 	    // Specify a path to the file you want to upload, or a Buffer
-	    .file( filePath )
+	    .file(filePath)
 	    .create({
 	        title: 'My awesome image',
 	        alt_text: 'an image of something awesome',
@@ -60,26 +39,18 @@ const addFeaturedImageToPost = function(id, socket) {
 	    })
 	    .then(function(response) {
 	        // Your media is now uploaded: let's associate it with a post
-	        // var newImageId = response.id;
-	        // return wp.media().id( newImageId ).update({
-	        //     post: id
-	        // });
-	        var newImageId = response.id;
+			const newImageId = response.id;
+			// console.log('here is the image id', newImageId);
 
-	        return wp.posts().id(id).update({
-    				featured_media: newImageId,
-    				status: 'publish',
-    				content: 'some lovely content'
-    			})
-	        	.then(function(response) {
-	        		console.log(response);
-	        		socket.emit('post complete');
-	        	});
-
-	    })
-	    .then(function( response ) {
-	            console.log( 'Media ID #' + response );
-	            // console.log( 'is now associated with Post ID #' + response.post );
-	        }
-	    );
+			const post = wp.posts().id(id).update({
+					featured_media: newImageId,
+					title: `test post`,
+					status: 'publish',
+					content: 'some lovely content'
+				})
+				.then(() => {
+					console.log('post posted');
+					// socket.emit('the post completed');
+				});
+	    });
 };
